3eda118561e310e0ee9ef109749135617347d6d7
repooley <122302315+repooley@users.noreply.github.com>
2025-01-24 08:46:09 -0700

    Updated labels, switched incorrectly named CPC3 and CPC10 variables, changed empty histogram boxes to white, changed histogram method from numpy to scipy to easily display median value in each bin and allow handling of NaNs, columns with NaNs are no longer dropped, and all CPC data is put in a single df before binning for simplicity

--- src/CurtainPlots/AltitudeLatitudeBinnedCurtainPlot.py
+++ src/CurtainPlots/AltitudeLatitudeBinnedCurtainPlot.py
@@ -11,6 +11,7 @@ import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
+from scipy.stats import binned_statistic_2d
 
 #########################
 ##--Open ICARTT Files--##
@@ -20,7 +21,7 @@ import matplotlib.pyplot as plt
 directory = r"C:\Users\repooley\REP_PhD\NETCARE2015\data"
 
 ##--Select flight (Flight1 thru Flight10)--##
-flight = "Flight2" ##--Flight1 AIMMS file currently broken at line 13234--##
+flight = "Flight2" # Flight1 AIMMS file currently broken at line 13234
 
 ##--Define function that creates datasets from filenames--##
 def find_files(directory, flight, partial_name):
@@ -33,8 +34,8 @@ def find_files(directory, flight, partial_name):
 aimms = icartt.Dataset(find_files(directory, flight, "AIMMS_POLAR6")[0])
 
 ##--CPC data--##
-CPC3 = icartt.Dataset(find_files(directory, flight, 'CPC3772')[0])
-CPC10 = icartt.Dataset(find_files(directory, flight, 'CPC3776')[0])
+CPC10 = icartt.Dataset(find_files(directory, flight, 'CPC3772')[0])
+CPC3 = icartt.Dataset(find_files(directory, flight, 'CPC3776')[0])
 
 #################
 ##--Pull data--##
@@ -140,54 +141,40 @@ for CPC10, T, P in zip(CPC10_conc_aligned, temperature, pressure):
 ##--Create 2D histogram--##
 ###########################
 
-##--Creates a Pandas dataframe for CPC3 data--##
-CPC3_df = pd.DataFrame({'Altitude': altitude, 'Latitude': latitude, 'CPC3_conc':CPC3_conc_STP})
+##--Creates a Pandas dataframe for CPC data--##
+CPC_df = pd.DataFrame({'Altitude': altitude, 'Latitude': latitude, 'CPC3_conc':CPC3_conc_STP, 'CPC10_conc': CPC10_conc_STP})
 
-##--Drop any rows in the dataframe where a NaN appears to make numpy histogram--##
-##--Created separate df for each CPC and nuc particles to preserve as much data as possible--##
-CPC3_clean_df = CPC3_df.dropna()
-
-##--Create dataframe for CPC10 data--##
-CPC10_df = pd.DataFrame({'Altitude':altitude, 'Latitude':latitude, 'CPC10_conc': CPC10_conc_STP})
-
-##--Drop NaNs--##
-CPC10_clean_df = CPC10_df.dropna()
-
-##--Create dataframe for nucleating particles (append later)--##
-nuc_df = pd.DataFrame({'Altitude': altitude, 'Latitude' :latitude, 'CPC3_conc' : CPC3_conc_STP, 'CPC10_conc': CPC10_conc_STP})
-
-##--Nucleation Mode Particles--##
-nuc_particles = (nuc_df['CPC3_conc'] - nuc_df['CPC10_conc'])
+##--Calculate N3-10 particles--##
+nuc_particles = (CPC_df['CPC3_conc'] - CPC_df['CPC10_conc'])
 
 ##--Change calculated particle counts less than zero to NaN--##
 nuc_particles = np.where(nuc_particles >= 0, nuc_particles, np.nan)
 
 ##--Append the dataframe with nucleated particles--##
-nuc_df['nuc_particles'] = nuc_particles
-
-##--Drop NaNs--##
-nuc_clean_df = nuc_df.dropna()
+CPC_df['nuc_particles'] = nuc_particles
 
 ##--Define number of bins here--##
-num_bins = 60
+num_bins = 30
 
-##--Compute 2D histogram for CPC3 (particle abundance in each altitude-latitude bin)--##
-CPC3_lat_bin_edges = np.linspace(CPC3_clean_df['Latitude'].min(), CPC3_clean_df['Latitude'].max(), num_bins + 1)
-CPC3_alt_bin_edges = np.linspace(CPC3_clean_df['Altitude'].min(), CPC3_clean_df['Altitude'].max(), num_bins + 1)
-CPC3_particle_abundance, CPC3_x_edges, CPC3_y_edges = np.histogram2d(CPC3_clean_df['Latitude'], CPC3_clean_df['Altitude'], 
-   bins=[CPC3_lat_bin_edges, CPC3_alt_bin_edges], weights=CPC3_clean_df['CPC3_conc'])
+##--Determine CPC3 bin edges--##
+CPC3_lat_bin_edges = np.linspace(CPC_df['Latitude'].min(), CPC_df['Latitude'].max(), num_bins + 1)
+CPC3_alt_bin_edges = np.linspace(CPC_df['Altitude'].min(), CPC_df['Altitude'].max(), num_bins + 1)
+
+##--Make 2d histogram for CPC3 and compute median particle abundance in each bin--##
+CPC3_bin_medians, CPC3_x_edges, CPC3_y_edges, _ = binned_statistic_2d(CPC_df['Latitude'], 
+    CPC_df['Altitude'], CPC_df['CPC3_conc'], statistic='median', bins=[CPC3_lat_bin_edges, CPC3_alt_bin_edges])
 
 ##--Histogram for CPC10--##
-CPC10_lat_bin_edges = np.linspace(CPC10_clean_df['Latitude'].min(), CPC10_clean_df['Latitude'].max(), num_bins + 1)
-CPC10_alt_bin_edges = np.linspace(CPC10_clean_df['Altitude'].min(), CPC10_clean_df['Altitude'].max(), num_bins +1)
-CPC10_particle_abundance, CPC10_x_edges, CPC10_y_edges = np.histogram2d(CPC10_clean_df['Latitude'], CPC10_clean_df['Altitude'],
-   bins=[CPC10_lat_bin_edges, CPC10_alt_bin_edges], weights=CPC10_clean_df['CPC10_conc'])
+CPC10_lat_bin_edges = np.linspace(CPC_df['Latitude'].min(), CPC_df['Latitude'].max(), num_bins + 1)
+CPC10_alt_bin_edges = np.linspace(CPC_df['Altitude'].min(), CPC_df['Altitude'].max(), num_bins +1)
+CPC10_bin_medians, CPC10_x_edges, CPC10_y_edges, _ = binned_statistic_2d(CPC_df['Latitude'], 
+    CPC_df['Altitude'], CPC_df['CPC10_conc'], statistic='median', bins=[CPC10_lat_bin_edges, CPC10_alt_bin_edges])
 
 ##--Histogram for nucleation mode particles--##
-nuc_lat_bin_edges = np.linspace(nuc_clean_df['Latitude'].min(), nuc_clean_df['Latitude'].max(), num_bins +1)
-nuc_alt_bin_edges = np.linspace(nuc_clean_df['Altitude'].min(), nuc_clean_df['Altitude'].max(), num_bins +1)
-nuc_particle_abundance, nuc_x_edges, nuc_y_edges = np.histogram2d(nuc_clean_df['Latitude'], nuc_clean_df['Altitude'], 
-   bins=[nuc_lat_bin_edges, nuc_alt_bin_edges], weights=nuc_clean_df['nuc_particles'])
+nuc_lat_bin_edges = np.linspace(CPC_df['Latitude'].min(), CPC_df['Latitude'].max(), num_bins +1)
+nuc_alt_bin_edges = np.linspace(CPC_df['Altitude'].min(), CPC_df['Altitude'].max(), num_bins +1)
+nuc_bin_medians, nuc_x_edges, nuc_y_edges, _ = binned_statistic_2d(CPC_df['Latitude'], 
+    CPC_df['Altitude'], CPC_df['nuc_particles'], statistic='median', bins=[nuc_lat_bin_edges, nuc_alt_bin_edges])
 
 ################
 ##--PLOTTING--##
@@ -196,18 +183,23 @@ nuc_particle_abundance, nuc_x_edges, nuc_y_edges = np.histogram2d(nuc_clean_df['
 ##--Particles larger than 3 nm--##
 fig1, ax1 = plt.subplots(figsize=(8, 6))
 
-# Use pcolormesh for the plot
-CPC3_plot = ax1.pcolormesh(CPC3_x_edges, CPC3_y_edges, CPC3_particle_abundance.T,  # Transpose to align correctly
-    shading='auto', cmap='viridis')
+##--Make special color map where 0 values are white--##
+new_cmap = plt.get_cmap('viridis')
+##--Values under specified minimum will be white--##
+new_cmap.set_under('w')
 
-# Add colorbar
+##--Use pcolormesh for the plot, set minimum value for viridis colors as 1--##
+CPC3_plot = ax1.pcolormesh(CPC3_x_edges, CPC3_y_edges, CPC3_bin_medians.T,  # Transpose to align correctly
+    shading='auto', cmap=new_cmap, vmin=1)
+
+##--Add colorbar--##
 cb = fig1.colorbar(CPC3_plot, ax=ax1)
-cb.set_label('Particles >3nm (Counts/cm^3)')
+cb.set_label('Particles >2.5 nm $(Counts/cm^{3})$', fontsize=12)
 
 # Set axis labels
-ax1.set_xlabel('Latitude (°)')
-ax1.set_ylabel('Altitude (m)')
-ax1.set_title(f"CPC3 Particle (>3nm) Abundance - {flight.replace('Flight', 'Flight ')}")
+ax1.set_xlabel('Latitude (°)', fontsize=12)
+ax1.set_ylabel('Altitude (m)', fontsize=12)
+ax1.set_title(f"Particles >2.5 nm Abundance - {flight.replace('Flight', 'Flight ')}")
 
 ##--Base output path in directory--##
 output_path = r"C:\Users\repooley\REP_PhD\NETCARE2015\data\processed\CurtainPlots\CPC3"
@@ -222,18 +214,18 @@ plt.show()
 ##--Particles larger than 10 nm--##
 fig2, ax2 = plt.subplots(figsize=(8, 6))
 
-# Use pcolormesh for the plot
-CPC10_plot = ax2.pcolormesh(CPC10_x_edges, CPC10_y_edges, CPC10_particle_abundance.T,  # Transpose to align correctly
-    shading='auto', cmap='viridis')
+##--Use pcolormesh for the plot, set minimum for viridis colors as 1--##
+CPC10_plot = ax2.pcolormesh(CPC10_x_edges, CPC10_y_edges, CPC10_bin_medians.T,  # Transpose to align correctly
+    shading='auto', cmap=new_cmap, vmin=1)
 
-# Add colorbar
+##--Add colorbar--##
 cb2 = fig2.colorbar(CPC10_plot, ax=ax2)
-cb2.set_label('Particles >10nm (Counts/cm^3)')
+cb2.set_label('Particles >10 nm $(Counts/cm^{3})$', fontsize=12)
 
-# Set axis labels
-ax2.set_xlabel('Latitude (°)')
-ax2.set_ylabel('Altitude (m)')
-ax2.set_title(f"CPC10 Particle (>10nm) Abundance - {flight.replace('Flight', 'Flight ')}")
+##--Set axis labels--##
+ax2.set_xlabel('Latitude (°)', fontsize=12)
+ax2.set_ylabel('Altitude (m)', fontsize=12)
+ax2.set_title(f"Particles >10 nm Abundance - {flight.replace('Flight', 'Flight ')}")
 
 ##--Base output path in directory--##
 output_path = r"C:\Users\repooley\REP_PhD\NETCARE2015\data\processed\CurtainPlots\CPC10"
@@ -248,18 +240,18 @@ plt.show()
 ##--Nucleating particles--##
 fig3, ax3 = plt.subplots(figsize=(8, 6))
 
-# Use pcolormesh for the plot
-nuc_plot = ax3.pcolormesh(nuc_x_edges, nuc_y_edges, nuc_particle_abundance.T,  # Transpose to align correctly
-    shading='auto', cmap='viridis')
+##--Use pcolormesh for the plot and use viridis for values greater than 1--##
+nuc_plot = ax3.pcolormesh(nuc_x_edges, nuc_y_edges, nuc_bin_medians.T,  # Transpose to align correctly
+    shading='auto', cmap=new_cmap, vmin=1)
 
-# Add colorbar
+##--Add colorbar--##
 cb3 = fig3.colorbar(nuc_plot, ax=ax3)
-cb3.set_label('Particles (Counts/cm^3)')
+cb3.set_label('2.5-10 nm Particles $(Counts/cm^{3})$', fontsize=12)
 
-# Set axis labels
-ax3.set_xlabel('Latitude (°)')
-ax3.set_ylabel('Altitude (m)')
-ax3.set_title(f"Nucleating Particle Abundance - {flight.replace('Flight', 'Flight ')}")
+##--Set axis labels--##
+ax3.set_xlabel('Latitude (°)', fontsize=12)
+ax3.set_ylabel('Altitude (m)', fontsize=12)
+ax3.set_title(f"2.5-10 nm Particle Abundance - {flight.replace('Flight', 'Flight ')}")
 
 ##--Base output path in directory--##
 output_path = r"C:\Users\repooley\REP_PhD\NETCARE2015\data\processed\CurtainPlots\Nucleating"
